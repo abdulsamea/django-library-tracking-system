f80c4ec6efbd176bb004ae38261fa4a47f242ed9
Abdul Samea <46262404+abdulsamea@users.noreply.github.com>
2025-10-16 22:15:20 +0530

    added first task

--- core_skills.py
+++ core_skills.py
@@ -1,6 +1,8 @@
 import random
-# rand_list =
+rand_list = [random.randint(1, 20) for _ in range(10)]
 
-# list_comprehension_below_10 =
+list_comprehension_below_10 = [num for num in rand_list if num < 10]
 
-# list_comprehension_below_10 =
+list_comprehension_below_10_with_filter = list(filter(lambda n: n < 10, rand_list))
+
+print(rand_list, list_comprehension_below_10, list_comprehension_below_10_with_filter)
