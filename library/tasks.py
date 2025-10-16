ae488a8bc8ecf7b7bcc19989749cf8c060899577
Abdul Samea <46262404+abdulsamea@users.noreply.github.com>
2025-10-16 22:31:27 +0530

    added second task for scheduling loan overdue via celery

--- library/tasks.py
+++ library/tasks.py
@@ -2,6 +2,7 @@ from celery import shared_task
 from .models import Loan
 from django.core.mail import send_mail
 from django.conf import settings
+from datetime import date
 
 @shared_task
 def send_loan_notification(loan_id):
@@ -17,4 +18,23 @@ def send_loan_notification(loan_id):
             fail_silently=False,
         )
     except Loan.DoesNotExist:
+        pass
+
+
+@shared_task
+def check_overdue_loans():
+    try:
+        overdue_loans = Loan.objects.filter(is_returned=False, due_date__lt=date.today())
+        for loan in overdue_loans:
+            member_email = loan.member.user.email
+            member_name = loan.member.user.first_name
+            book_title = loan.book.title
+            send_mail(
+                subject='Reminder for overdue books',
+                message=f'Hello {member_name},\n\nYou have loan overdue for the booth: "{book_title}".\nPlease pay the loan for the book.',
+                from_email=settings.DEFAULT_FROM_EMAIL,
+                recipient_list=[member_email],
+                fail_silently=False,
+            )
+    except:
         pass
